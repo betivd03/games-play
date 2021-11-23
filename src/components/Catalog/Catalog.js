f3e70a919b07c75355c3801c7959f3cd2d9f36ab
Beatris Borislavova <betivd03@abv.bg>
2021-11-23 23:13:41 +0200

    Show No games yet

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -4,22 +4,43 @@ import Card from "./Card.js";
 
 const Catalog = () => {
     const [games, setGames] = useState([]);
+    // const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        // setLoading(true);
+
+        // setTimeout(() => {
+        //     fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+        //         .then(res => res.json())
+        //         .then(result => {
+        //             setGames(result);
+        //             setLoading(false);
+        //         });
+        // }, 1000);
+
         fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
             .then(res => res.json())
             .then(result => {
                 setGames(result);
-            })
+            });
     }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            
-            { games.map(x => <Card game={x} />) }
 
-            <h3 className="no-articles">No games yet</h3>
+            {/* { loading 
+                ? <h3 className="no-articles">Loading...</h3>
+                : games.length > 0
+                    ? games.map(x => <Card key={x._id} game={x} />)
+                    : <h3 className="no-articles">No games yet</h3>
+            } */}
+
+            { games.length > 0
+                ? games.map(x => <Card key={x._id} game={x} />)
+                : <h3 className="no-articles">No games yet</h3>
+            }
+            
         </section>
     );
 };
